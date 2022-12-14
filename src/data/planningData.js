a41b218e431aa3862e49502a6bb11113d74d8782
Julio Punkito <julesnoir@hotmail.fr>
2022-12-14 10:16:33 +0100

    Création d'un composant image viewer

--- src/data/planningData.js
+++ src/data/planningData.js
@@ -4,7 +4,7 @@ export const planningData = [
   {
     date: { day: days[0], hour: "10H" },
     title: "test",
-    peoples: ["John Doe", "Martine Lagruette"],
+    peoples: ["Rififi Lacharcut"],
     description: "lorem ipsum blabla",
   },
   {

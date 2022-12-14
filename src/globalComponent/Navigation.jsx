46946d714abb7f693b1086fdf63a64d8ade24e2e
remi.lopez <r.lopez76000@gmail.com>
2022-12-14 16:52:00 +0100

    logo size

--- src/globalComponent/Navigation.jsx
+++ src/globalComponent/Navigation.jsx
@@ -16,6 +16,7 @@ import RoomIcon from "@mui/icons-material/Room";
 import { ticketLink } from "../../globalData";
 import LocalActivityIcon from "@mui/icons-material/LocalActivity";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
+import CloseIcon from "@mui/icons-material/Close";
 const Navigation = ({ classes, handleThemeChange }) => {
   const [drawer, setDrawer] = React.useState({
     right: false,
@@ -62,7 +63,7 @@ const Navigation = ({ classes, handleThemeChange }) => {
 
     return (
       <Box
-        sx={{ width: "60vw" }}
+        sx={{ width: "100vw" }}
         role="presentation"
         onClick={toggleDrawer(currentDrawerDirection, false)}
         onKeyDown={toggleDrawer(currentDrawerDirection, false)}
@@ -74,6 +75,21 @@ const Navigation = ({ classes, handleThemeChange }) => {
                 <DarkModeIcon />
               </ListItemIcon>
               <ListItemText>Theme clair/sombre</ListItemText>
+            </ListItemButton>
+            <ListItemButton onClick={handleThemeChange}>
+              <ListItemIcon style={{ minWidth: 0 }}>
+              <CloseIcon
+          style={{
+            fontSize: "1.75em",
+            position: "fixed",
+            top: "0.25em",
+            right: ".25em",
+            zIndex: 2,
+            cursor: "pointer",
+          }}
+          onClick={() => toggleDrawer(currentDrawerDirection, false)}
+        />
+              </ListItemIcon>
             </ListItemButton>
           </ListItem>
           {linkData.map((data, index) => (
@@ -118,14 +134,14 @@ const Navigation = ({ classes, handleThemeChange }) => {
   return (
     <nav>
       <img
-        style={{ width: "3em" }}
+        style={{ width: "4.25em" }}
         src={ window.localStorage["currentTheme"] === "dark"? "/images/logo_asso.png" : "/images/logo_asso_blanc.png"}
         alt="Logo de rpgameFest"
       />
       <Button onClick={toggleDrawer(currentDrawerDirection, true)}>
         <MenuIcon />
       </Button>
-      <Drawer
+      <Drawer 
         anchor={currentDrawerDirection}
         open={drawer[currentDrawerDirection]}
         onClose={toggleDrawer(currentDrawerDirection, false)}

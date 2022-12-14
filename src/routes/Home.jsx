97234cdbad8f954d819397b3b48df2b0b6b49cb3
Julio Punkito <julesnoir@hotmail.fr>
2022-12-14 16:10:16 +0100

    Fix home src url

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     <h1 className="enchanted" style={{fontSize:"2em",textAlign:"left",marginBlockStart:"0.5em",marginBlockEnd:"0"}} >Edition 2022</h1>
     <p style={{marginBlockStart:"0.5em"}}>Rpgaming Fest est présent cette année au Parc Exposition de Rouen. Retrouvez nous <span style={{fontWeight:"bold"}}>le 21 et 22 Janvier 2022.</span></p>
     <div style={{display:"flex",gap:".6em", justifyContent:"space-between"}}>
-    <img style={{width:"10em"}} src="images/parcExpo-removebg-preview.png" alt="Logo du parc exposition" />
+    <img style={{width:"10em"}} src="/images/parcExpo-removebg-preview.png" alt="Logo du parc exposition" />
     <div style={{display:"flex",flexDirection:"column", gap:".8em", justifyContent:"center"}}> 
     <Button variant="contained" className="enchanted" style={{backgroundColor:"#3B6B90",width:"100%",fontSize:"1.2em"}}><Link style={{color:"white"}}to="/planning">Programme</Link></Button>
     <Button variant="contained" className="enchanted" style={{backgroundColor:"#3B6B90",width:"150px",fontSize:"1.2em"}}><Link style={{color:"white"}}to="/plan">Plan</Link></Button>
